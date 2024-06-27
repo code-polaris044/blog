47f5fe84c2090e71b234b020e6cf4483f2c0dfcb
shun916 <118881278+code-polaris044@users.noreply.github.com>
2024-06-28 08:14:57 +0900

    [fix]アイコン
    表示できたが、色々お問題があるので再度学習

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { siteMeta } from "./src/lib/constants";
+const { siteUrl } = siteMeta;
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: siteUrl,
+    trailingSlash: "always",
+});
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,9 @@
 export const siteMeta = {
     siteTitle: "Shell Design",
     siteDesc: "Shell Designのブログサイト",
+    siteUrl: "http://localhost:4321",
+    siteType: "webdsite",
+    siteLocale: "ja_Jp",
+    siteIcon: "/favicon.ico",
+    siteImg: "/ogp.jpg",
 };
