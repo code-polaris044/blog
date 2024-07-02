276b5f76088f367423517b5cad7512143b131330
s-sugawara-alistyle <142774009+s-sugawara-alistyle@users.noreply.github.com>
2024-07-02 15:33:39 +0900

    [fix]画像の最適化は次回対応予定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from "astro/config";
 import { siteMeta } from "./src/lib/constants";
-const { siteUrl } = siteMeta;
 import icon from "astro-icon";
+import image from "@astrojs/image";
+
+const {
+  siteUrl
+} = siteMeta;
+
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteUrl,
-    integrations: [icon()],
+  site: siteUrl,
+  integrations: [
+    icon(), 
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    })
+  ]
 });
