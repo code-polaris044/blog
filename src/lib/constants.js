46d98802b07ad85386a2ab2310a4791285159561
s-sugawara-alistyle <142774009+s-sugawara-alistyle@users.noreply.github.com>
2024-07-02 13:32:43 +0900

    [fix]

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -7,3 +7,8 @@ export const siteMeta = {
     siteIcon: "/favicon.ico",
     siteImg: "/ogp.jpg",
 };
+
+export const categories = [
+	{ categoryName: 'Python', categorySlug: 'python' },
+	{ categoryName: 'JavaScript', categorySlug: 'javascript' },
+];
