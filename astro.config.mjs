19d14627f351e16b66a57098d8adb5b43b58597c
s-sugawara-alistyle <142774009+s-sugawara-alistyle@users.noreply.github.com>
2024-07-03 13:18:49 +0900

    [fix]mdxに置換

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from "astro-icon";
 import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 const {
   siteUrl
 } = siteMeta;
@@ -14,5 +15,5 @@ export default defineConfig({
   site: siteUrl,
   integrations: [icon(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), prefetch(), sitemap()]
+  }), prefetch(), sitemap(), mdx()]
 });
