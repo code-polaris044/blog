f7120d08638333e2a0138ee04dda46772b805af0
shun916 <118881278+code-polaris044@users.noreply.github.com>
2024-09-17 04:47:23 +0900

    [add]非同期ページ遷移

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/static";
+// import swup from "@swup/astro"; 学習が必要
 
 const { siteUrl } = siteMeta;
 
@@ -23,6 +24,12 @@ export default defineConfig({
         sitemap(),
         mdx(),
         react(),
+        // swup({
+        //     theme: false, //デフォルトで用意されている遷移アニメーションは使用しない
+        //     smoothScrolling: false, //スムーズスクロール無効化
+        //     updateBodyClass: true, //ページ遷移時にbody要素のクラス名を更新する
+        //     reloadScripts: false, //遷移後にスクリプトを再読み込みさせない
+        // }),
     ],
     output: "static",
     adapter: vercel({
